24b1c76cd4b36e642bbaac10c42e7deec5887c46
Laven Morrow <lmorrow13@live.com>
2017-10-04 17:21:38 -0600

    fixes for problem three

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ function devide(l,e){
 
 var cararray = [['Vanquish','Valkyrie','DB9'],['325i','X5','3 Series'],['Flying Spur','Mulsanne','Bentayga']];
 console.log(cararray[2][1]);
+
+//Problem 3
+
+var num = prompt('What is your favorite number?');
+if(parseInt(num) < 100){
+	alert('Less than 100')
+}else{
+	parseInt((num) > 100)
+	alert('More than 100')
+}
