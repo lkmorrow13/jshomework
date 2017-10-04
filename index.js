7f936ae8195ad263d6e585b5fc43dfebeffe160f
Laven Morrow <lmorrow13@live.com>
2017-10-04 16:40:57 -0600

    problem one fixes

---
+++ index.js
@@ -0,0 +1,16 @@
+//Problem 1
+function math(n,m) {
+	var sum = n + m;
+	console.log(sum);
+}math(2,9)
+
+function times(b,r){
+	var sum = b * r;
+	console.log(sum)
+}times(9,5)
+
+function devide(l,e){
+	var sum = l / e;
+	console.log(sum)
+}devide(4,2)
+
